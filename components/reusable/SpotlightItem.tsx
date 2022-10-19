19f14f61c4481c977c088d9f121d6774da545316
Josh Warren <joshnwarren@gmail.com>
2022-10-18 22:11:22 -0700

    spotlight click effect

--- components/reusable/SpotlightItem.tsx
+++ components/reusable/SpotlightItem.tsx
@@ -17,6 +17,9 @@ const Wrapper = styled(motion.div)<{ css: FlattenSimpleInterpolation }>`
   overflow: hidden;
   :hover {
     border: 1px solid var(--border-color-primary);
+    > div > div {
+      opacity: 0.25;
+    }
   }
   ${({ css }) => css}
 `
@@ -29,24 +32,31 @@ const SpotLightContainer = styled.div`
   justify-content: center;
   top: 0;
   left: 0;
-  z-index: 5;
-  filter: blur(30px);
-  :hover {
-    > div {
-      opacity: 1;
-    }
-  }
+  z-index: -15;
+  filter: blur(30px) saturate(0.85);
 `
-
 const SpotLight = styled(motion.div)`
-  background: radial-gradient(#ffffff2b 0%, transparent);
+  background: radial-gradient(
+    closest-side,
+    rgba(255, 255, 255, 1) 50%,
+    rgba(255, 0, 0, 1) 55%,
+    rgba(255, 154, 0, 1) 60%,
+    rgba(208, 222, 33, 1) 65%,
+    rgba(79, 220, 74, 1) 70%,
+    rgba(63, 218, 216, 1) 75%,
+    rgba(47, 201, 226, 1) 80%,
+    rgba(28, 127, 238, 1) 85%,
+    rgba(95, 21, 242, 1) 90%,
+    rgba(186, 12, 248, 1) 95%,
+    rgba(251, 7, 217, 1) 98%,
+    transparent 100%
+  );
   opacity: 0;
   position: absolute;
   pointer-events: none;
   width: 50%;
   height: 100%;
   z-index: -1;
-  transition: opacity 0.25s ease-in-out;
 `
 
 export const SpotlightItem: FC<
@@ -58,8 +68,9 @@ export const SpotlightItem: FC<
     }
 > = ({ children, css, link, ...props }) => {
   const [coords, setCoords] = useState({ x: 0, y: 0 })
-  const [scale, setScale] = useState(1)
+  const [scale, setScale] = useState(0.8)
   const [hover, setHover] = useState(false)
+  const [tap, setTap] = useState(false)
   const ref = React.useRef<HTMLDivElement>(null)
   const handleMouseMove = (e: React.MouseEvent) => {
     const element = ref.current
@@ -73,20 +84,26 @@ export const SpotlightItem: FC<
     const newScale = Math.sqrt(x * x + y * y * 15) / (width / 4)
     setScale(newScale > 0.8 ? newScale : 0.8)
   }
+
   const Inner = (
-    <Wrapper css={css} {...props}>
-      <SpotLightContainer
-        ref={ref}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        onMouseMove={handleMouseMove}
-      >
+    <Wrapper
+      ref={ref}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => (setHover(false), setTap(false))}
+      onMouseMove={handleMouseMove}
+      onMouseDown={() => setTap(true)}
+      css={css}
+      {...props}
+    >
+      {children}
+      <SpotLightContainer>
         <SpotLight
-          transition={{ type: `spring`, damping: 40, stiffness: 300 }}
-          animate={{ x: coords.x, y: coords.y, scale: scale }}
+          initial="initial"
+          animate="animate"
+          variants={variants}
+          custom={{ coords, scale, hover, tap }}
         />
       </SpotLightContainer>
-      {children}
     </Wrapper>
   )
 
@@ -96,4 +113,28 @@ export const SpotlightItem: FC<
       {Inner}
     </Link>
   )
+}
+
+interface VariantProps {
+  coords: { x: number; y: number }
+  scale: number
+  hover: boolean
+  tap: boolean
+}
+const variants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: ({ coords, scale, tap, hover }: VariantProps) => ({
+    x: coords.x,
+    y: coords.y,
+    scale: scale,
+    opacity: tap ? 0.75 : hover ? 0.25 : 0,
+    transition: {
+      type: `spring`,
+      damping: 22,
+      stiffness: 300,
+      opacity: { damping: 5, stiffness: 9999 },
+    },
+  }),
 }
